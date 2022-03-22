d12ab055e26c6d2564f827d90706d5f5bd260f05
Gonçalo Ribeiro <goncalocribeiro@tecnico.ulisboa.pt>
2022-03-22 17:38:16 +0000

    init springboot-config-server

---
+++ src/test/java/com/example/springbootconfigserver/SpringbootConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
